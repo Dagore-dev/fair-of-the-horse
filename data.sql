5fdf62e1dd20faab75d33c51ec1a032ad1fb4e1d
Dagore <david.gomez.redondo.dev@gmail.com>
2022-05-04 14:06:00 +0200

    first commit

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO STREET VALUES (1, 'La paquera de Jerez');
+INSERT INTO BOOTH VALUES (1, 'Caseta La Alegria', 100, 1);
+INSERT INTO BOOTH VALUES (2, 'Caseta inventada', 100, 1);
+INSERT INTO MEMBER VALUES (1, 'Javier', 'Casetero', '112233449');
+INSERT INTO MEMBER_OF VALUES (1, 1, 5);
+INSERT INTO awards VALUES(1,'The most happy', 1);
