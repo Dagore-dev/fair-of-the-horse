ae3760721fb3bb804212cb68f7137e33a14d5778
Dagore <david.gomez.redondo.dev@gmail.com>
2022-05-12 21:59:23 +0200

    fixed bug in award_list

--- views/award_list.sql
+++ views/award_list.sql
@@ -1,16 +1,18 @@
 -- Create a view that shows the description of the prize (prize), the name of the booth (booth) and its location, indicating street and number separated by a comma (location).
 CREATE OR REPLACE VIEW award_list
 (
-  award,
   description,
-  booth
+  booth,
+  location
 )
 AS
 SELECT
-  award_id,
   description,
-  CONCAT(CONCAT(owner, ', '), id_booth)
+  booth.name,
+  CONCAT(CONCAT(street.name, ', '), booth.street_id_street)
 FROM awards
 JOIN booth
-  ON booth_id_booth = id_booth;
+  ON booth_id_booth = id_booth
+JOIN street
+  ON street_id_street = id_street;
   
